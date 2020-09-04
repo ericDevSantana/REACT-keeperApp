a89bb403e88e12185ca05a0d80584ef64abae5fc
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-04 16:05:07 -0700

    Arrow Functions, Adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+// function createNote(note){
+//    return(
+//       <Note
+//       key={note.id}
+//       title={note.title}
+//       content={note.content}
+//       />
+//    );
+// }
 
 function App() {
    return <div>
     <Header/>
-    <Note/>
+    {notes.map((note) => (
+      <Note
+         key={note.id}
+         title={note.title}
+         content={note.content}
+      />
+   ))}
     <Footer/>
    </div>    
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Note() {
-    return <div className="note">
-        <h1>Title</h1>
-        <p>This is the note content.</p>
-    </div>
-}
+// function Note(props) {
+//     return (<div className="note">
+//         <h1>{props.title}</h1>
+//         <p>{props.content}</p>
+//     </div>)
+// }
 
-export default Note;
+export default (props) => (<div className="note">
+<h1>{props.title}</h1>
+<p>{props.content}</p>
+</div>)
