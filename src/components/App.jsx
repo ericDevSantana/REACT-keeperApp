0574014642a84a83945fe83fc30968ed951bba1c
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-09 17:31:34 -0700

    Add Notes funcionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,28 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+   const [allNotes, setNewNote] = React.useState([]);
+
+   function addNote(note) {
+      // Add object Note to allNotes
+
+      setNewNote((prevNotes) => {
+         return [...prevNotes, note]
+      });
+   }
+
    return <div>
     <Header/>
-    <CreateArea />
-   <Note key={1} title="Note title" content="Note content" />
+    <CreateArea onADD={addNote}/>
+    
+    {allNotes.map((note, index) => (
+       
+      <Note
+              key={index}
+              title={note.title}
+              content={note.content}/>
+      ))}
+
     <Footer/>
    </div>    
 }
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = React.useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event){
+    event.preventDefault();
+    const {name, value} = event.target;
+    
+    setNote((prevValue) => {
+        return {
+          ...prevValue,
+          [name]: value
+        };
+    });
+  }
+
+  function submitNote(event){
+    props.onADD(note);
+    setNote({title:"", content:""});
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
